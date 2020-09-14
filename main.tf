2e9d7120570d9c80ac9d78a1f149700d7cfdcb38
Greg Moy <gregmoy@users.noreply.github.com>
2020-09-14 13:09:10 -0700

    force vpc

--- main.tf
+++ main.tf
@@ -33,6 +33,7 @@ resource "aws_network_interface" "this" {
 
 resource "aws_eip" "this" {
   count             = var.enabled ? var.eip_creation ? 1 : 0 : 0
+  vpc               = true
   network_interface = aws_network_interface.this.id
   tags              = local.common_tags
 }
